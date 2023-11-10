66ddb6c992cc44a64d4cbe14912b8e14eda38278
anilates97 <anilates.97@gmail.com>
2023-11-10 03:49:34 +0300

    sidebar designed

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -47,3 +47,12 @@ const variations = {
     }
   `,
 };
+
+const Button = styled.button`
+  border: none;
+  border-radius: var(--border-radius-sm);
+
+  box-shadow: var(--shadow-sm);
+`;
+
+export default Button;
