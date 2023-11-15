f3e1c5abac58e0deb5a641e8bb9eed89ac207419
anilates97 <anilates.97@gmail.com>
2023-11-15 05:51:25 +0300

    builded header

--- src/features/authentication/useLogin.js
+++ src/features/authentication/useLogin.js
@@ -11,7 +11,7 @@ export function useLogin() {
     mutationFn: ({ email, password }) => loginApi({ email, password }),
     onSuccess: (user) => {
       //user datasını manuel olarak cache'e atma
-      queryClient.setQueriesData(["user"], user);
+      queryClient.setQueryData(["user"], user.user);
       navigate("/dashboard", { replace: true });
     },
     onError: (err) => {
